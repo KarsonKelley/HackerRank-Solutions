219b21ad576c2ca27a72131f068dfaddae5a74d4
KarsonKelley <karsonk1113@gmail.com>
2023-06-05 14:08:55 -0500

    Second solution

--- random-challenges/generating-x.py
+++ random-challenges/generating-x.py
@@ -1,6 +1,7 @@
 numbers = [5,2,5,2,2]
-for index in numbers:
-    for value in range(index):
-        print("x",end='')
-    print()
+for x_count in numbers:
+    output = ""
+    for value in range(x_count):
+        output += "x"
+    print(output)
     
