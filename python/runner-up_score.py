2d44f8144baee05ec6a47d8e9e98f77074e59fee
KarsonKelley <karsonk1113@gmail.com>
2023-06-04 19:16:53 -0500

    Updated

--- python/runner-up_score.py
+++ python/runner-up_score.py
@@ -1,16 +1,16 @@
 n = 5
 a, b, c, d, e = 5, 3, 6, 2, 2 #sample inputs
-scores_ar = [a,b,c,d,e]
+arr = [a,b,c,d,e]
 
-scores_ar.sort()
-runner_up = scores_ar[n-1]
+arr.sort()
+runner_up = arr[n-1]
 while n > 0:
-    if scores_ar[n-1] < runner_up:
-        runner_up = scores_ar[n-1]
+    if arr[n-1] < runner_up:
+        runner_up = arr[n-1]
         break
-    n-=1
+    n = n-1
 print(runner_up)
-
+    
 
 
 
