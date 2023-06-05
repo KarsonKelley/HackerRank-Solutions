4b5a71437887a0ab664a3ff79182ac19e9245cdd
KarsonKelley <karsonk1113@gmail.com>
2023-06-04 20:30:59 -0500

    Completed challenge

---
+++ python/diagonal-difference.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    first_sum = 0
+    last_sum = 0
+    i = 0
+    while i < len(arr):
+        first_sum +=arr[i][i]
+        last_sum += arr[i][len(arr)-1-i]
+        i+=1
+    return abs(first_sum - last_sum)
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
